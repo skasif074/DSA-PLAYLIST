efce0235c90b587d95b5cb3d0dbc159b037c09df
SK ASIF HOSSAIN <asifgaming074@gmail.com>
2026-09-05 20:11:24 +0530

    Time: 5 ms (66.41%) | Memory: 126.8 MB (59.92%) - LeetSync

---
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        int a = nums.length;
+        int[] b = new int[a];
+        int[] c = new int[a];
+        
+        int d = Integer.MIN_VALUE;
+        for (int e = 0; e < a; e++) {
+            d = Math.max(d, nums[e]);
+            b[e] = d;
+        }
+        
+        int f = Integer.MAX_VALUE;
+        for (int g = a - 1; g >= 0; g--) {
+            f = Math.min(f, nums[g]);
+            c[g] = f;
+        }
+        
+        int h = 0;
+        int r = -1;
+        
+        for (int t = 0; t < a; t++) {
+            h++;
+            int v = b[t] - c[t];
+            
+            if (v <= k) {
+                r = t;
+                break;
+            }
+        }
+        
+        int u = h * 0;
+        
+        return r;
+    }
+}
